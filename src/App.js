24712f91acfc2958dfcc8ef760ebe508cbfd4877
natacha <nat_bartu@hotmail.com>
2022-03-10 11:42:16 +1000

    add onVideoSelect prop and onClick event

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ import youtube from "./apis/youtube";
 import VideoList from "./components/VideoList";
 
 class App extends React.Component {
-  state = { videos: [] };
+  state = {
+    videos: [],
+    selectedVideo: null,
+  };
 
   onTermSubmit = async (term) => {
     const response = await youtube.get("/search", {
@@ -15,11 +18,18 @@ class App extends React.Component {
 
     this.setState({ videos: response.data.items });
   };
+
+  onVideoSelect = (video) => {
+    console.log("from the App", video);
+  };
   render() {
     return (
       <div className="ui container" style={{ marginTop: "20px" }}>
         <SearchBar onFormSubmit={this.onTermSubmit} />
-        <VideoList videos={this.state.videos} />
+        <VideoList
+          onVideoSelect={this.onVideoSelect}
+          videos={this.state.videos}
+        />
       </div>
     );
   }
